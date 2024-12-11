df1f9eb9dd4599fdd00958f2a6fe391283d3c42c
Nicolas Pedranti <nicopedranti@gmail.com>
2024-12-11 14:43:58 -0300

    feat: add mobile menu

--- script.js
+++ script.js
@@ -24,54 +24,125 @@ $form.addEventListener('submit', (event) => checkFormValues(event))
 
 // API functions
 
-async function getCoffee() {
+const coffees = []
+let totalInCart = 0
 
+async function getCoffee() {
   try {
-    const response = await fetch("https://fake-coffee-api.vercel.app/api")
+    const response = await fetch('https://fake-coffee-api.vercel.app/api')
 
     if (!response.ok) {
-      throw new Error (`HTTP error! Status: ${response.status}`)
+      throw new Error(`HTTP error! Status: ${response.status}`)
     }
     const products = await response.json()
-  
-    const $divProductos = document.querySelector('.productos')
-  
-    products.forEach(({id, name, description, price, image_url}) => {
-      $divProductos.innerHTML += `
-                <div class="producto">
-                  <img src=${image_url} alt="${name} image">
-                  <h2>${name}</h2>
-                  <h3>${description}</h3>
-                  <p>${price.toLocaleString('es-AR', { style: "currency", currency: 'ARS'})}</p>
-                  <button onClick="addToCart(${id})">Agregar al carrito</button>
-                </div>
-      `
-    }) 
 
-    // getCart()
+    const $divProductos = document.querySelector('.productos')
+
+    products.forEach((coffee) => {
+      coffees.push(coffee)
+
+      console.log(coffees)
+
+      const $product = document.createElement('div')
+
+      $product.classList.add('producto')
+
+      $product.innerHTML += `
+                          <img src=${coffee.image_url} alt="${coffee.name} image">
+                          <h2>${coffee.name}</h2>
+                          <h3>${coffee.description}</h3>
+                          <p>${coffee.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS'})}</p>
+                          <button>Agregar al carrito</button>
+      `
+
+      $divProductos.appendChild($product)
+
+      // Event to button
+      const $coffeeButton = $product.querySelector('button')
+
+      $coffeeButton.addEventListener('click', (event) => addToCart(event, coffee))
+
+      // Event to modal
+      $product.addEventListener('click', () => {
+        const $modal = document.getElementById('modal')
+
+        $modal.classList.add('show-modal')
+
+        $modal.firstElementChild.innerHTML = `
+                          <svg id="close-button" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+                          <img src=${coffee.image_url} alt="${coffee.name} image">
+                          <h2>${coffee.name}</h2>
+                          <h3>${coffee.description}</h3>
+                          <p>${coffee.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS'})}</p>
+                          <button>Agregar al carrito</button>
+      `
+        $modal.addEventListener('click', () => $modal.classList.remove('show-modal'))
+
+        const $closeButton = document.getElementById('close-button')
+        $closeButton.addEventListener('click', () => $modal.classList.remove('show-modal'))
+
+        const $modalButton = $modal.querySelector('button')
+        $modalButton.addEventListener('click', (event) => addToCart(event, coffee))
+
+        const $modalCard = document.querySelector('.modal-card')
+        $modalCard.addEventListener('click', (event) => event.stopPropagation())
+      })
+    })
+
+    getCart()
   } catch (error) {
-    console.error("Error en la comunicación con la API:", error)
+    console.error('Error en la comunicación con la API:', error)
   }
 }
 
 document.addEventListener('DOMContentLoaded', getCoffee())
 
 function getCart() {
-  let cart = JSON.parse(localStorage.getItem("cart")) || []
+  const cart = JSON.parse(localStorage.getItem("cart")) || []
 
   const cartCounter = document.getElementById('cart-counter')
 
-  cartCounter.textContent = cart.length
+  if (cart.length > 0) {
+    let totalCount = 0
+
+    cart.forEach(({stock}) => totalCount += stock)
+
+    cartCounter.textContent = totalCount
+  }
 }
 
-function addToCart(id) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || []
+function addToCart(event, coffeeToAdd) {
+  event.stopPropagation()
 
-  cart.push(id);
+  const storagedCart = JSON.parse(localStorage.getItem("cart")) || []
 
-  localStorage.setItem("cart", JSON.stringify(cart))
+  const coffeeFoundInStorage = storagedCart.find(coffee => coffee.id === coffeeToAdd.id)
+
+  if (coffeeFoundInStorage) {
+    coffeeFoundInStorage.stock ++ 
+  } else {
+    storagedCart.push({...coffeeToAdd, stock: 1})
+  }
+  
+  localStorage.setItem("cart", JSON.stringify(storagedCart))
 
   console.log("Producto añadido al carrito")
   
-  // getCart()
+  getCart()
 }
+
+// NAV Events
+
+const $menuButton = document.getElementById('mobile-menu-button')
+
+$menuButton.addEventListener('click', () => {
+  if ($menuButton.nextElementSibling.classList.contains('hide')) {
+    $menuButton.nextElementSibling.classList.remove('hide')
+  } else {
+    $menuButton.nextElementSibling.classList.add('hide')
+  }
+})
+
+const $mobileMenu = document.getElementById('mobile-menu-list')
+
+// $mobileMenu. // TODO OCULTAR EL MENU AL TOCAR CUALQUIER ITEM
